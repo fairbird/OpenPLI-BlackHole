fd02c3169ade0d5c5b8dce047ef48a70f25bd3d4
RAED <rrrr53@hotmail.com>
2017-06-03 19:38:26 +0300

    Update HistoryZapSelector

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from Tools.Profile import profile
 from Screen import Screen
 import Screens.InfoBar
@@ -37,14 +38,13 @@ from Screens.RdsDisplay import RassInteractive
 from ServiceReference import ServiceReference
 from Tools.BoundFunction import boundFunction
 from Tools import Notifications
-from Tools.Alternatives import CompareWithAlternatives
+from Tools.Alternatives import CompareWithAlternatives, GetWithAlternative
 from Tools.Directories import fileExists
 from time import localtime, time
 from Plugins.Plugin import PluginDescriptor
 from Components.PluginComponent import plugins
 from Screens.ChoiceBox import ChoiceBox
 from Screens.EventView import EventViewEPGSelect
-from Tools.Alternatives import GetWithAlternative
 import os, unicodedata
 from Blackhole.BhEpgSearch import Nab_EpgSearch, Nab_EpgSearchLast
 profile('ChannelSelection.py after imports')
@@ -2073,20 +2073,23 @@ class ChannelSelection(ChannelSelectionBase, ChannelSelectionEdit, ChannelSelect
     def addToHistory(self, ref):
         if self.delhistpoint is not None:
             x = self.delhistpoint
-            while x <= len(self.history) - 1:
+            while x <= len(self.history)-1:
                 del self.history[x]
-
         self.delhistpoint = None
+
         if self.servicePath is not None:
             tmp = self.servicePath[:]
             tmp.append(ref)
-            try:
-                del self.history[self.history_pos + 1:]
-            except:
-                pass
-
             self.history.append(tmp)
             hlen = len(self.history)
+            x = 0
+            while x < hlen - 1:
+                if self.history[x][-1] == ref:
+                    del self.history[x]
+                    hlen -= 1
+                else:
+                    x += 1
+
             if hlen > HISTORYSIZE:
                 del self.history[0]
                 hlen -= 1
@@ -2101,15 +2104,16 @@ class ChannelSelection(ChannelSelectionBase, ChannelSelectionEdit, ChannelSelect
         if hlen > 1 and self.history_pos > 0:
             self.history_pos -= 1
             self.setHistoryPath()
-        self.delhistpoint = self.history_pos + 1
+        self.delhistpoint = self.history_pos+1
 
     def historyNext(self):
+        self.delhistpoint = None
         hlen = len(self.history)
         if hlen > 1 and self.history_pos < hlen - 1:
             self.history_pos += 1
             self.setHistoryPath()
 
-    def setHistoryPath(self, doZap = True):
+    def setHistoryPath(self, doZap=True):
         path = self.history[self.history_pos][:]
         ref = path.pop()
         del self.servicePath[:]
@@ -2119,8 +2123,9 @@ class ChannelSelection(ChannelSelectionBase, ChannelSelectionEdit, ChannelSelect
         cur_root = self.getRoot()
         if cur_root and cur_root != root:
             self.setRoot(root)
+        self.servicelist.setCurrent(ref)
         if doZap:
-            self.session.nav.playService(ref, adjust=False)
+            self.session.nav.playService(ref)
         if self.dopipzap:
             self.setCurrentSelection(self.session.pip.getCurrentService())
         else:
@@ -2129,45 +2134,38 @@ class ChannelSelection(ChannelSelectionBase, ChannelSelectionEdit, ChannelSelect
 
     def historyClear(self):
         if self and self.servicelist:
-            for i in range(0, len(self.history) - 1):
+            for i in range(0, len(self.history)-1):
                 del self.history[0]
-
-            self.history_pos = len(self.history) - 1
+            self.history_pos = len(self.history)-1
             return True
         return False
 
     def historyZap(self, direction):
         hlen = len(self.history)
-        if hlen < 1:
-            return
+        if hlen < 1: return
         mark = self.history_pos
         selpos = self.history_pos + direction
-        if selpos < 0:
-            selpos = 0
-        if selpos > hlen - 1:
-            selpos = hlen - 1
+        if selpos < 0: selpos = 0
+        if selpos > hlen-1: selpos = hlen-1
         serviceHandler = eServiceCenter.getInstance()
-        historylist = []
+        historylist = [ ]
         for x in self.history:
             info = serviceHandler.info(x[-1])
-            if info:
-                historylist.append((info.getName(x[-1]), x[-1]))
-
+            if info: historylist.append((info.getName(x[-1]), x[-1]))
         self.session.openWithCallback(self.historyMenuClosed, HistoryZapSelector, historylist, selpos, mark, invert_items=True, redirect_buttons=True, wrap_around=True)
 
     def historyMenuClosed(self, retval):
-        if not retval:
-            return
+        if not retval: return
         hlen = len(self.history)
         pos = 0
         for x in self.history:
-            if x[-1] == retval:
-                break
+            if x[-1] == retval: break
             pos += 1
-
-        self.delhistpoint = pos + 1
+        self.delhistpoint = pos+1
         if pos < hlen and pos != self.history_pos:
             tmp = self.history[pos]
+            # self.history.append(tmp)
+            # del self.history[pos]
             self.history_pos = pos
             self.setHistoryPath()
 
@@ -2557,8 +2555,8 @@ class SimpleChannelSelection(ChannelSelectionBase, SelectionEventInfo):
 
 
 class HistoryZapSelector(Screen):
-
-    def __init__(self, session, items = [], sel_item = 0, mark_item = 0, invert_items = False, redirect_buttons = False, wrap_around = True):
+    def __init__(self, session, items=None, sel_item=0, mark_item=0, invert_items=False, redirect_buttons=False, wrap_around=True):
+        if not items: items = []
         Screen.__init__(self, session)
         self.redirectButton = redirect_buttons
         self.invertItems = invert_items
@@ -2566,77 +2564,64 @@ class HistoryZapSelector(Screen):
             self.currentPos = len(items) - sel_item - 1
         else:
             self.currentPos = sel_item
-        self['actions'] = ActionMap(['OkCancelActions', 'InfobarCueSheetActions'], {'ok': self.okbuttonClick,
-         'cancel': self.cancelClick,
-         'jumpPreviousMark': self.prev,
-         'jumpNextMark': self.next,
-         'toggleMark': self.okbuttonClick})
-        self.setTitle(_('History zap...'))
+        self["actions"] = ActionMap(["OkCancelActions", "InfobarCueSheetActions"],
+            {
+                "ok": self.okbuttonClick,
+                "cancel": self.cancelClick,
+                "jumpPreviousMark": self.prev,
+                "jumpNextMark": self.next,
+                "toggleMark": self.okbuttonClick,
+            })
+        self.setTitle(_("History zap..."))
         self.list = []
         cnt = 0
         serviceHandler = eServiceCenter.getInstance()
         for x in items:
+
             info = serviceHandler.info(x[-1])
             if info:
-                orbpos = self.getOrbitalPos(ServiceReference(x[1]))
                 serviceName = info.getName(x[-1])
                 if serviceName is None:
-                    serviceName = ''
-                eventName = ''
-                descriptionName = ''
-                durationTime = ''
+                    serviceName = ""
+                eventName = ""
+                descriptionName = ""
+                durationTime = ""
+                # if config.plugins.SetupZapSelector.event.value != "0":
                 event = info.getEvent(x[-1])
                 if event:
                     eventName = event.getEventName()
                     if eventName is None:
-                        eventName = ''
+                        eventName = ""
                     else:
-                        eventName = eventName.replace('(18+)', '').replace('18+', '').replace('(16+)', '').replace('16+', '').replace('(12+)', '').replace('12+', '').replace('(7+)', '').replace('7+', '').replace('(6+)', '').replace('6+', '').replace('(0+)', '').replace('0+', '')
+                        eventName = eventName.replace('(18+)', '').replace('18+', '').replace('(16+)', '').replace('16+', '').replace('(12+)', '').replace('12+', '').replace('(7+)', '').replace('7+', '').replace('(6+)', '').replace('6+', '').replace('(0+)', '').replace('0+', '') 
+                    # if config.plugins.SetupZapSelector.event.value == "2":
                     descriptionName = event.getShortDescription()
-                    if descriptionName is None or descriptionName == '':
+                    if descriptionName is None or descriptionName == "":
                         descriptionName = event.getExtendedDescription()
                         if descriptionName is None:
-                            descriptionName = ''
+                            descriptionName = ""
+                    # if config.plugins.SetupZapSelector.duration.value:
                     begin = event.getBeginTime()
                     if begin is not None:
                         end = begin + event.getDuration()
                         remaining = (end - int(time())) / 60
-                        prefix = ''
+                        prefix = ""
                         if remaining > 0:
-                            prefix = '+'
+                            prefix = "+"
                         local_begin = localtime(begin)
                         local_end = localtime(end)
-                        durationTime = _('%02d.%02d - %02d.%02d (%s%d min)') % (local_begin[3],
-                         local_begin[4],
-                         local_end[3],
-                         local_end[4],
-                         prefix,
-                         remaining)
-            png = ''
+                        durationTime = _("%02d.%02d - %02d.%02d (%s%d min)") % (local_begin[3],local_begin[4],local_end[3],local_end[4],prefix, remaining)
+
+            png = ""
             picon = getPiconName(str(ServiceReference(x[1])))
-            if picon != '':
+            if picon != "":
                 png = loadPNG(picon)
             if self.invertItems:
-                self.list.insert(0, (x[1],
-                 cnt == mark_item and '\xc2\xbb' or '',
-                 x[0],
-                 eventName,
-                 descriptionName,
-                 durationTime,
-                 png,
-                 orbpos))
+                self.list.insert(0, (x[1], cnt == mark_item and "»" or "", x[0], eventName, descriptionName, durationTime, png))
             else:
-                self.list.append((x[1],
-                 cnt == mark_item and '\xc2\xbb' or '',
-                 x[0],
-                 eventName,
-                 descriptionName,
-                 durationTime,
-                 png,
-                 orbpos))
+                self.list.append((x[1], cnt == mark_item and "»" or "", x[0], eventName, descriptionName, durationTime, png))
             cnt += 1
-
-        self['menu'] = List(self.list, enableWrapAround=wrap_around)
+        self["menu"] = List(self.list, enableWrapAround=wrap_around)
         self.onShown.append(self.__onShown)
 
     def __onShown(self):
