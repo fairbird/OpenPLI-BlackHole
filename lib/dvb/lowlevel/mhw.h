19a0d7ba5954d5b84bb5e2add94344cc39f2544b
RAED <rrrr53@hotmail.com>
2017-06-22 05:50:20 +0300

    Remove ugly HILO and SET_HILO defines

--- lib/dvb/lowlevel/mhw.h
+++ lib/dvb/lowlevel/mhw.h
@@ -86,6 +86,9 @@ typedef struct {
    u_char mhw2_mjd_lo                            :8;
    u_char mhw2_duration_hi                       :8;
    u_char mhw2_duration_lo                       :8;
+
+   int getDuration() const     { return duration_hi << 8 | duration_lo; };
+   int getMhw2Duration() const { return mhw2_duration_hi << 8 | mhw2_duration_lo; };
 //Nabilosat
    u_char title_ext [33];
 //end
